fb8c82bee068fbcb1010def555b28fa8c7e0386d
Ignatiusgoh <ignatiusgoh97@gmail.com>
2025-06-19 10:04:23 +0800

    small edit

--- src/utils/supabasehelpers.py
+++ src/utils/supabasehelpers.py
@@ -11,8 +11,6 @@ load_dotenv()
 # Supabase credentials
 supabase_url = os.getenv("SUPABASE_URL")
 supabase_key = os.getenv("SUPABASE_API_KEY")
-print(supabase_url)
-print(supabase_key)
 supabase = create_client(supabase_url, supabase_key)
 
 def analyze_trades():
