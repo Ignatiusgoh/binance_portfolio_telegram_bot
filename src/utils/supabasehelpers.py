d5ae0d3a07bd3cff20a6e606f92c3c0d2f48e0c7
Ignatiusgoh <ignatiusgoh97@gmail.com>
2025-06-19 09:49:17 +0800

    small edit

--- src/utils/supabasehelpers.py
+++ src/utils/supabasehelpers.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 # Supabase credentials
 supabase_url = os.getenv("SUPABASE_URL")
-supabase_key = os.getenv("SUPABASE_KEY")
+supabase_key = os.getenv("SUPABASE_API_KEY")
 print(supabase_url)
 print(supabase_key)
 supabase = create_client(supabase_url, supabase_key)
