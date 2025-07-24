b6b730c475b483a4c1826e630062adc6879cbb8c
Ignatiusgoh <ignatiusgoh97@gmail.com>
2025-07-24 14:30:14 +0800

    accounted for 2 different binance accounts

--- src/utils/binancehelpers.py
+++ src/utils/binancehelpers.py
@@ -11,26 +11,39 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Binance API Keys
-api_key = os.getenv('BINANCE_API_KEY')
-api_secret = os.getenv('BINANCE_API_SECRET')
-client = Client(api_key=api_key, api_secret=api_secret)
+api_key_strat_1 = os.getenv('BINANCE_API_KEY_STRAT_1')
+api_secret_strat_1 = os.getenv('BINANCE_API_SECRET_STRAT_1')
+api_key_strat_2 = os.getenv('BINANCE_API_KEY_STRAT_1')
+api_secret_strat_2 = os.getenv('BINANCE_API_SECRET_STRAT_1')
+
+client_strat_1 = Client(api_key=api_key_strat_1, api_secret=api_secret_strat_1)
+client_strat_2 = Client(api_key=api_key_strat_2, api_secret=api_secret_strat_2)
 
 def get_usdt_balance():
     while True:
         try:
-            futures_account = client.futures_account()
-            for asset in futures_account['assets']:
+            futures_account_strat_1 = client_strat_1.futures_account()
+            for asset in futures_account_strat_1['assets']:
                 if asset['asset'] == 'USDT':
-                    return float(asset['marginBalance'])
+                    strat_1_usdt_balance = float(asset['marginBalance'])
+            
+            futures_account_strat_2 = client_strat_2.futures_account()
+            for asset in futures_account_strat_2['assets']:
+                if asset['asset'] == 'USDT':
+                    strat_2_usdt_balance = float(asset['marginBalance'])
+            return strat_1_usdt_balance, strat_2_usdt_balance
         except requests.exceptions.RequestException as e:
             logging.warning(f"⚠️ Error fetching balance: {e}. Retrying")
             time.sleep(0.1)
 
 def get_open_positions():
     try:
-        positions = client.futures_account()['positions']
-        open_positions = [p for p in positions if float(p['positionAmt']) != 0]
-        return open_positions
+        positions_strat_1 = client_strat_1.futures_account()['positions']
+        open_positions_strat_1 = [p for p in positions_strat_1 if float(p['positionAmt']) != 0]
+        positions_strat_2 = client_strat_2.futures_account()['positions']
+        open_positions_strat_2 = [p for p in positions_strat_2 if float(p['positionAmt']) != 0]
+
+        return open_positions_strat_1, open_positions_strat_2
     except Exception as e:
         logging.warning(f"⚠️ Error fetching positions: {e}")
         return []
--- src/utils/telegramhelpers.py
+++ src/utils/telegramhelpers.py
@@ -22,15 +22,19 @@ async def handle_custom_buttons(update: Update, context: ContextTypes.DEFAULT_TY
 
     if text == "📊 Show current portfolio balance":
         balance = get_usdt_balance()
-        await update.message.reply_text(f"💰 Current USDT Balance: {balance:.2f}")
+        await update.message.reply_text(f"💰 STRATEGY 1 Current USDT Balance: {balance[0]:.2f}")
+        await update.message.reply_text(f"💰 STRATEGY 2 Current USDT Balance: {balance[1]:.2f}")
 
     elif text == "📈 Show open positions":
         positions = get_open_positions()
-        if not positions:
-            await update.message.reply_text("📭 No open positions.")
+        if not positions[0]:
+            await update.message.reply_text("📭 STRATEGY 1 No open positions.")
+        if not positions[1]:
+            await update.message.reply_text("📭 STRATEGY 2 No open positions.")
         else:
-            msg = "\n".join([f"{p['symbol']}: {p['positionAmt']} @ {p['entryPrice']}" for p in positions])
-            await update.message.reply_text(f"📈 Open Positions:\n{msg}")
+            msg_1 = "\n".join([f"STRATEGY 1 {p['symbol']}: {p['positionAmt']} @ {p['entryPrice']}" for p in positions[0]])
+            msg_2 = "\n".join([f"STRATEGY 2 {p['symbol']}: {p['positionAmt']} @ {p['entryPrice']}" for p in positions[1]])
+            await update.message.reply_text(f"📈 Open Positions:\n{msg_1}\n{msg_2}")
 
     elif text == "📋 Show trade statistics":
         stats = analyze_trades()
